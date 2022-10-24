2a3e260044e8ab1768ea872a4ff007ae5e122dcd
elise bell <eliseraynahasawsomesauce@gmail.com>
2022-10-24 11:16:21 -0400

    states list loads

--- Scripts/parkSrch.js
+++ Scripts/parkSrch.js
@@ -1,2 +1,19 @@
 "use strict";
 
+const statesList = document.getElementById("states-List");
+
+
+function loadStatesList() {
+    locationsArray.forEach((location) => {
+        let option = new Option(location);
+        statesList.appendChild(option);
+    });
+}
+
+function displayParkInformation() {
+    
+}
+
+window.onload = () => {
+    loadStatesList();
+}
