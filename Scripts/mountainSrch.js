475214f1ca99c6f0964321e4d4fa6b8822b00ed1
elise bell <eliseraynahasawsomesauce@gmail.com>
2022-10-26 11:38:20 -0400

    mountain dropdown functional

--- Scripts/mountainSrch.js
+++ Scripts/mountainSrch.js
@@ -1,2 +1,14 @@
 "use strict";
 
+const mountainDropdown = document.getElementById("mountian-dropdown-list");
+
+function loadMountainList() {
+    mountainsArray.forEach(mountain => {
+        let option = new Option(mountain.name, mountain.name);
+        mountainDropdown.appendChild(option);
+    });
+}
+
+window.onload = () => {
+    loadMountainList();
+  };
